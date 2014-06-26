60a79f054c0d765951f438cfa5452e75d925688e
Ronny Eichler <ronny.eichler@gmail.com>
2014-06-26 23:40:30 +0200

    continued work on new conversion script

--- continuous2dat.py
+++ continuous2dat.py
@@ -20,15 +20,84 @@ class SessionReader(object):
 def gather_files(in_dirs, channels, proc_node):
     print in_dirs
     base_names = [os.path.join(path, '{proc_node:d}_CH{channel:d}.continuous') for path in in_dirs]
-    return base_names
+    fnames = []
+    for fname_base in base_names:
+        print channels
+        fnames.extend([fname_base.format(proc_node=proc_node, channel=chan) for chan in channels])
+    return fnames 
 
+def read_header(filename):
+    """Return dict with raw files header content."""
+    # TODO: Use alternative reading method when filename is a file id instead of filename
+
+    # 1 kiB header string
+    header_dt = np.dtype([('Header', 'S%d', % SIZE_HEADER)])
+    header = np.fromfile(filename, dtype=header_dt, count=1)
+
+    # alternative which moves file pointer
+    #fid = open(filename, 'rb')
+    #header = fid.read(SIZE_HEADER)
+    #fid.close()
+
+    # Stand back! I know regex!
+    # Annoyingly, there is a newline character missing in the header
+    regex = "header\.([\d\w\.\s]{1,}).=.\'*([^\;\']{1,})\'*"
+    header_str = str(header[0][0]).rstrip(' ')
+    header_dict = {group(0): group[1] for group in re.compile(regex).findall(header_str)}
+    for key in ['bitVolts', 'sampleRate']:
+        header_dict[key] = float(header_dict[key])
+    for key in ['blockLength', 'bufferSize', 'header_bytes', 'channel']:
+        header_dict[key] = int(header_dict[key] if not key == 'channel' else header_dict[key][2:])
+    return header_dict
+
+def read_records(filename, offset=0, count=10000):
+    with open(filename, 'rb') as fid:
+        # move pointer to new position in file
+        fid.seek(SIZE_HEADER + offset*SIZE_RECORD)
+
+        # data type of individual records, n Bytes
+        data_dt = np.dtype([('timestamp', np.int64),
+                            ('n_samples', np.uint16),
+                            ('rec_num', np.uint16),
+                            # note endian type
+                            ('samples', ('>i2', NUM_SAMPLES)),
+                            ('rec_mark', (np.uint8, 10))])
+
+       return np.fromfile(fid, dtype=data_dt, count=count)
+
+def check_data(data):
+    """Sanity checks of records."""
+
+    # Timestamps should increase monotonically in steps of 1024
+    assert len(set(np.diff(data['timestamp']))) == 1 and np.diff(data['timestamp'][:2]) == 1024
+    print 'timestamps: ', data['timestamp'][0]
+
+    # Number of samples in each record should be NUM_SAMPLES, or 1024
+    assert len(set(data['n_samples'])) == 1 and data['n_samples'][0] == NUM_SAMPLES
+    print 'N samples: ', data['n_samples'][0]
+
+    # should be byte pattern [0...8, 255]
+    markers = set(map(str, data['rec_mark']))  # <- slow
+    assert len(markers) == 1 and str(REC_MARKER) in markers
+    print 'record marker: ', data['rec_mark'][0]
+
+    # should be zero, or there are multiple recordings in this file
+    assert len(set(data['rec_num'])) == 1 and data['rec_num'][0] == 0
+    print 'Number recording: ', data['rec_num'][0]
+
+
+def data_to_buffer(file_path, channels, count=1000, proc_node=100, buf=None):
+    """Read [count] records from [proc_node] file at [filepath] into a buffer."""
+    assert channels is Iterable
+    
+    # temporary storage
+    buf = np.zeros((len(channels), count*SIZE_NUM_SAMPLES
 
 def continuous_to_dat(in_dir, outfile, channels, proc_node=100, chunk_size=10000):
-    files = gather_files(in_dir, channels, proc_node
-    fnames = [fname_base.format(proc_node=proc_node, channel=chan+1) for chan in channels]
-    for file in files:
-        print 'Is {file} an existing file?'.format(file=file)
-        assert os.path.isfile(files)
+    files = gather_files(in_dir, channels, proc_node)
+    for f in files:
+        print 'Is {file} an existing file? {existance}'.format(file=f, existance='YES' if os.path.isfile(f) else 'NO!')
+        assert os.path.isfile(f)
 
     return files
     with open(outfile, 'wb'):
@@ -44,10 +113,15 @@ if __name__ == "__main__":
                                 definition file.
                                 Listing multiple files will result in data sets being merged in listed order.""")
 
-    parser.add_argument("-c", "--channels",
-                        nargs='*', default=64,
-                        help="""Number of channels, or comma-separated list of channels to merge from the
-                                .probe file""")
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument("-c", "--channel_list",
+                        nargs='*', default=[],
+                        type=int,
+                        help="""List of channels to merge from the .probe file""")
+
+    group.add_argument("-C", "--channel-count", default=64, type=int,
+                       help="Number of consecutive channels, 1-based index. E.g. 64: Channels 1:64")
 
     parser.add_argument("-l", "--layout", help="Path to .probe file.")
     parser.add_argument("-p", "--params", help="Path to .params file.")
@@ -57,4 +131,8 @@ if __name__ == "__main__":
                         help="Output file path.")
     args = parser.parse_args()
     print args
-    print continuous_to_dat(args.target, args.output, args.output)
+    channels = args.channel_list if args.channel_list else list(range(1, args.channel_count+1))
+
+    print continuous_to_dat(in_dir=args.target,
+                            outfile=args.output,
+                            channels=channels)
