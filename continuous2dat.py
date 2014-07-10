f816a7979e0a0ecd47aea735da463eb4f5f04c2e
Ronny Eichler <ronny.eichler@gmail.com>
2014-07-10 21:16:46 +0200

    Cleanup and moving things around. Whoop whoop.

--- continuous2dat.py
+++ continuous2dat.py
@@ -7,9 +7,17 @@ Created on Jun 15, 2014 23:40
 Reads Open Ephys .continuous files and converts them to raw 16-bit .dat files readable by
 "old" analysis tools (KlustaKwik, Spike_Detekt, Klusters, Neuroscope)
 """
+from __future__ import division
 import os
+import re
 import numpy as np
 from collections import Iterable
+
+
+SIZE_HEADER = 1024  # size of header in B
+NUM_SAMPLES = 1024  # number of samples per record
+SIZE_RECORD = 2070  # total size of record (2x1024 B samples + record header)
+REC_MARKER = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 255], dtype=np.uint8)
 
 
 class SessionReader(object):
@@ -17,27 +25,20 @@ class SessionReader(object):
         self.channels = channels if channels is Iterable else range(channels)
         self.files = {set_id: files for set_id, files in enumerate(target)}
 
-def gather_files(in_dirs, channels, proc_node):
-    print in_dirs
-    base_names = [os.path.join(path, '{proc_node:d}_CH{channel:d}.continuous') for path in in_dirs]
-    fnames = []
-    for fname_base in base_names:
-        print channels
-        fnames.extend([fname_base.format(proc_node=proc_node, channel=chan) for chan in channels])
+
+def gather_files(input_directories, channels, proc_node):
+    base_names = [os.path.join(path, '{proc_node:d}_CH{channel:d}.continuous') for path in input_directories]
+    fnames = [fname_base.format(proc_node=proc_node, channel=chan) for chan in channels for fname_base in base_names]
     return fnames 
 
-def read_header(filename):
-    """Return dict with raw files header content."""
-    # TODO: Use alternative reading method when filename is a file id instead of filename
+
+def read_header(filename, size_header=SIZE_HEADER):
+    """Return dict with .continuous file header content."""
+    # TODO: Compare headers, should be identical except for channel
 
     # 1 kiB header string
-    header_dt = np.dtype([('Header', 'S%d', % SIZE_HEADER)])
-    header = np.fromfile(filename, dtype=header_dt, count=1)
-
-    # alternative which moves file pointer
-    #fid = open(filename, 'rb')
-    #header = fid.read(SIZE_HEADER)
-    #fid.close()
+    header_dt = np.dtype([('Header', 'S%d' % size_header)])
+    header = read_segment(filename, offset=0, count=1, dtype=header_dt)
 
     # Stand back! I know regex!
     # Annoyingly, there is a newline character missing in the header
@@ -50,20 +51,33 @@ def read_header(filename):
         header_dict[key] = int(header_dict[key] if not key == 'channel' else header_dict[key][2:])
     return header_dict
 
-def read_records(filename, offset=0, count=10000):
+
+def read_records(filename, record_offset=0, record_count=10000,
+                 size_header=SIZE_HEADER, num_samples=NUM_SAMPLES, size_record=SIZE_RECORD):
+
+    # data type of individual records, n Bytes              # (2048 + 22) Byte = 2070 Byte total
+    data_dt = np.dtype([('timestamp', np.int64),            # 8 Byte
+                        ('n_samples', np.uint16),           # 2 Byte
+                        ('rec_num', np.uint16),             # 2 Byte
+                        ('samples', ('>i2', num_samples)),  # 2 Byte each x 1024; note endian type
+                        ('rec_mark', (np.uint8, 10))])      # 10 Byte
+
+    return read_segment(filename, offset=size_header + record_offset*size_record, count=record_count, dtype=data_dt)
+
+
+def read_segment(filename, offset, count, dtype):
+    # TODO: Use alternative reading method when filename is a file id instead of filename
+        # alternative which moves file pointer
+        #fid = open(filename, 'rb')
+        #data = fid.read(STUFF)
+        #fid.close()
+
     with open(filename, 'rb') as fid:
-        # move pointer to new position in file
-        fid.seek(SIZE_HEADER + offset*SIZE_RECORD)
+        fid.seek(offset)
+        segment = np.fromfile(fid, dtype=dtype, count=count)
 
-        # data type of individual records, n Bytes
-        data_dt = np.dtype([('timestamp', np.int64),
-                            ('n_samples', np.uint16),
-                            ('rec_num', np.uint16),
-                            # note endian type
-                            ('samples', ('>i2', NUM_SAMPLES)),
-                            ('rec_mark', (np.uint8, 10))])
+    return segment
 
-       return np.fromfile(fid, dtype=data_dt, count=count)
 
 def check_data(data):
     """Sanity checks of records."""
@@ -86,12 +100,24 @@ def check_data(data):
     print 'Number recording: ', data['rec_num'][0]
 
 
-def data_to_buffer(file_path, channels, count=1000, proc_node=100, buf=None):
+def data_to_buffer(file_path, channels, count=1000, proc_node=100, buf=None, size_record=SIZE_RECORD):
     """Read [count] records from [proc_node] file at [filepath] into a buffer."""
     assert channels is Iterable
     
     # temporary storage
-    buf = np.zeros((len(channels), count*SIZE_NUM_SAMPLES
+    buf = buf if buf is not None else np.zeros((len(channels), count * size_record), dtype='>i2')
+
+    # load chunk of data from all channels
+    # for n in channels:
+    #     filename = file_path+base_end.format(proc_node=proc_node, channel=n+1)
+    #     # channel offset is clunky shortcut for plotting
+    #     if channel_offset:
+    #         buf[n] = read_records(filename, count=count)['samples'].ravel() + channel_offset * (n-32)
+    #     else:
+    #         buf[n] = read_records(filename, count=count)['samples'].ravel()
+
+    return buf
+
 
 def continuous_to_dat(in_dir, outfile, channels, proc_node=100, chunk_size=10000):
     files = gather_files(in_dir, channels, proc_node)
@@ -100,10 +126,11 @@ def continuous_to_dat(in_dir, outfile, channels, proc_node=100, chunk_size=10000
         assert os.path.isfile(f)
 
     return files
-    with open(outfile, 'wb'):
-        pass
+    # with open(outfile, 'wb'):
+    #     pass
 
 if __name__ == "__main__":
+
     # Command line interface
     import argparse
     parser = argparse.ArgumentParser()
@@ -116,9 +143,9 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
 
     group.add_argument("-c", "--channel_list",
-                        nargs='*', default=[],
-                        type=int,
-                        help="""List of channels to merge from the .probe file""")
+                       nargs='*', default=[],
+                       type=int,
+                       help="""List of channels to merge from the .probe file""")
 
     group.add_argument("-C", "--channel-count", default=64, type=int,
                        help="Number of consecutive channels, 1-based index. E.g. 64: Channels 1:64")
