256ad93468eb45be17869faa05d07127946f224d
wonkoderverstaendige <ronny.eichler@gmail.com>
2014-07-14 13:24:48 +0200

    File gathering list

--- continuous2dat.py
+++ continuous2dat.py
@@ -27,28 +27,37 @@ class SessionReader(object):
 
 
 def gather_files(input_directories, channels, proc_node):
+    """Return list of paths to valid input files from list of input directories."""
+    #TODO: Handling non-existing items in file list (i.e. directories named like .continuous files)
+
     base_names = [os.path.join(path, '{proc_node:d}_CH{channel:d}.continuous') for path in input_directories]
-    fnames = [fname_base.format(proc_node=proc_node, channel=chan) for chan in channels for fname_base in base_names]
-    return fnames 
+    filenames = [fname_base.format(proc_node=proc_node, channel=chan) for chan in channels for fname_base in base_names]
+
+    for f in filenames:
+        print 'Is {file} an existing file? {existance}'.format(file=f, existance='YES' if os.path.isfile(f) else 'NO!')
+        assert os.path.isfile(f)
+
+    return filenames
 
 
 def read_header(filename, size_header=SIZE_HEADER):
     """Return dict with .continuous file header content."""
     # TODO: Compare headers, should be identical except for channel
 
-    # 1 kiB header string
+    # 1 kiB header string data type
     header_dt = np.dtype([('Header', 'S%d' % size_header)])
     header = read_segment(filename, offset=0, count=1, dtype=header_dt)
 
     # Stand back! I know regex!
-    # Annoyingly, there is a newline character missing in the header
+    # Annoyingly, there is a newline character missing in the header (version/header_bytes)
     regex = "header\.([\d\w\.\s]{1,}).=.\'*([^\;\']{1,})\'*"
     header_str = str(header[0][0]).rstrip(' ')
-    header_dict = {group(0): group[1] for group in re.compile(regex).findall(header_str)}
+    header_dict = {group[0]: group[1] for group in re.compile(regex).findall(header_str)}
     for key in ['bitVolts', 'sampleRate']:
         header_dict[key] = float(header_dict[key])
     for key in ['blockLength', 'bufferSize', 'header_bytes', 'channel']:
         header_dict[key] = int(header_dict[key] if not key == 'channel' else header_dict[key][2:])
+
     return header_dict
 
 
@@ -66,6 +75,7 @@ def read_records(filename, record_offset=0, record_count=10000,
 
 
 def read_segment(filename, offset, count, dtype):
+    """Read segment of a file from [offset] for [count]x[dtype]"""
     # TODO: Use alternative reading method when filename is a file id instead of filename
         # alternative which moves file pointer
         #fid = open(filename, 'rb')
@@ -119,15 +129,27 @@ def data_to_buffer(file_path, channels, count=1000, proc_node=100, buf=None, siz
     return buf
 
 
-def continuous_to_dat(in_dir, outfile, channels, proc_node=100, chunk_size=10000):
+def folders_to_dat(in_dir, outfile, channels, proc_node=100, *args, **kwargs):
+    """Given list of input directories [in_dir] will write out .continuous files for channels in [channels] iterable
+    of an open ephys [proc_node] into single [outfile] .dat file.
+    """
+    #TODO: out_log file should be ast.eval()-uable (i.e. exported as dict)
+
     files = gather_files(in_dir, channels, proc_node)
-    for f in files:
-        print 'Is {file} an existing file? {existance}'.format(file=f, existance='YES' if os.path.isfile(f) else 'NO!')
-        assert os.path.isfile(f)
+    log_string = '{in_dir}:\nChannels: {channels}, proc_node: {proc_node}\n\n'
+    try:
+        with open(outfile, 'wb') as out_fid_dat, open(outfile+'.log', 'w') as out_fid_log:
+            out_fid_log.write(log_string.format(in_dir=in_dir, channels=str(channels), proc_node=proc_node))
+
+            for f in files:
+                out_fid_log.write(f+':\n'+str(read_header(f))+'\n\n')
+                #out_fid_dat.write()
+
+    except IOError as e:
+        print 'Operation failed: %s' % e.strerror
 
     return files
-    # with open(outfile, 'wb'):
-    #     pass
+
 
 if __name__ == "__main__":
 
@@ -154,12 +176,12 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--params", help="Path to .params file.")
 
     parser.add_argument("-o", "--output",
-                        default='./proc/raw.dat',
+                        default='raw.dat',
                         help="Output file path.")
     args = parser.parse_args()
     print args
     channels = args.channel_list if args.channel_list else list(range(1, args.channel_count+1))
 
-    print continuous_to_dat(in_dir=args.target,
+    print folders_to_dat(in_dir=args.target,
                             outfile=args.output,
                             channels=channels)
