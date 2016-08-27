7e0be00a287eff6268a4dd6c316de0c67c1acf6e
Ronny Eichler <r.eichler@science.ru.nl>
2016-08-28 01:17:38 +0200

    Better time string formatting

--- oio/util.py
+++ oio/util.py
@@ -71,12 +71,22 @@ def fmt_channel_ranges(channels, shorten_seq=5, rs="tm", cs="_", zp=2):
     return cs.join(range_strings)
 
 
-def fmt_time(s):
+def fmt_time(s, minimal=True):
+    """
+    Args:
+        s: time in seconds (float for fractionals)
+        minimal: Flag, if true, only return strings for times > 0, leave rest outs
+    Returns: String formatted 99h 59min 59.9s, where elements < 1 are left out optionally.
+
+    """
     ms = s-int(s)
     s = int(s)
+    if s < 60 and minimal:
+        return "{s:02.3f}s".format(s=s+ms)
+
     m, s = divmod(s, 60)
-    if m < 60:
-        return "{m:02d}min{s:02.3f}s".format(m=m, s=s+ms)
-    else:
-        h, m = divmod(m, 60)
-        return "{h:02d}h{m:02d}min{s:02.3f}s".format(h=h, m=m, s=s+ms)
+    if m < 60 and minimal:
+        return "{m:02d}min {s:02.3f}s".format(m=m, s=s+ms)
+
+    h, m = divmod(m, 60)
+    return "{h:02d}h {m:02d}min {s:02.3f}s".format(h=h, m=m, s=s+ms)
